23c244436e3796bb67f2f5015a383fffde0ea9f7
bhosalerushi2001 <bhosalerushi2001@gmail.com>
2024-05-15 09:54:34 +0530

    Add project files.

---
+++ EmployeeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUDAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUDAPP
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("DBCS")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CRUDAPP.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "Address must be between 5 and 200 characters")]
+        public string Address { get; set; }
+
+    }
+}
